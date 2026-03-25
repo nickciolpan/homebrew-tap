8ca5153202e6a732d24fa12407d2ff3b8ae9149d
Nick Ciolpan <nick@graffino.com>
2026-03-25 08:55:22 +0200

    Update docker-scan-lite to v1.1.0

--- Formula/docker-scan-lite.rb
+++ Formula/docker-scan-lite.rb
@@ -1,13 +1,13 @@
 class DockerScanLite < Formula
   desc "Lightweight Docker image scanner for security analysis"
   homepage "https://github.com/nickciolpan/docker-scan-lite"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.0.0/docker-scan-lite-darwin-arm64.tar.gz"
-      sha256 "d0c99db730f65caa52c3c3e495f14a83e0fc0880f770968e48d4dcdad8d9ca60"
+      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.1.0/docker-scan-lite-darwin-arm64.tar.gz"
+      sha256 "b2165205b263877607d9cde56ec9517050fafaf899b2f4afef2ba29568f4373c"
 
       def install
         bin.install "docker-scan-lite-darwin-arm64" => "docker-scan-lite"
@@ -15,8 +15,8 @@ class DockerScanLite < Formula
     end
 
     on_intel do
-      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.0.0/docker-scan-lite-darwin-amd64.tar.gz"
-      sha256 "35c8e34e2a356f728fe3aed2fb2a9a8efd4fdd473786651ac4bfef0fa05f73ca"
+      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.1.0/docker-scan-lite-darwin-amd64.tar.gz"
+      sha256 "42ac9a508cc58d6d478e81a62e08150a0c15851baf7cb91170de83b641548cac"
 
       def install
         bin.install "docker-scan-lite-darwin-amd64" => "docker-scan-lite"
@@ -26,8 +26,8 @@ class DockerScanLite < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.0.0/docker-scan-lite-linux-arm64.tar.gz"
-      sha256 "c8cb7c9aa6c484814e4eb2087f620f6800dc7487f84ad2bf5b3356d18f80800d"
+      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.1.0/docker-scan-lite-linux-arm64.tar.gz"
+      sha256 "00c1d4398399e2b2f0d50318596628f624e08b74ed403167ce2ba24eb0b2f5d1"
 
       def install
         bin.install "docker-scan-lite-linux-arm64" => "docker-scan-lite"
@@ -35,8 +35,8 @@ class DockerScanLite < Formula
     end
 
     on_intel do
-      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.0.0/docker-scan-lite-linux-amd64.tar.gz"
-      sha256 "a9866ac26478e5d20965a120f47c5e32593618306bf5f8aa7b863cda7738a4db"
+      url "https://github.com/nickciolpan/docker-scan-lite/releases/download/v1.1.0/docker-scan-lite-linux-amd64.tar.gz"
+      sha256 "c1b02c453f6ef2a147cfc98a366cff39d6065693f16e0d22787e30175ed8dfb2"
 
       def install
         bin.install "docker-scan-lite-linux-amd64" => "docker-scan-lite"
